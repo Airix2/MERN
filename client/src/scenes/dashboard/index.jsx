4e0db93d4dd466bad8f3214973191884c2ad2eb2
Airix2 <alex.soto@cetys.edu.mx>
2022-12-30 16:50:30 -0800

    start commit

---
+++ client/src/scenes/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import { useMediaQuery } from "@mui/material";
+import React from "react";
+import { useGetDashboardQuery } from "state/api";
+
+const Dashboard = () => {
+	const theme = useTheme();
+	const isNonMediumScreens = useMediaQuery("(min-width:1200px)");
+	const { data, isLoading } = useGetDashboardQuery();
+
+	return <div>Dashboard</div>;
+};
+
+export default Dashboard;
